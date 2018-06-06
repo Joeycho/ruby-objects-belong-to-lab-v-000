87a9bcb74f3cdf8db94d5648f79ee681785ebdbd
Joey Cho <cjy12182000@naver.com>
2018-06-06 09:19:15 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,7 +2,7 @@ class Post
   attr_accessor :title
   attr_accessor :Author
 
-  def initialize(title, author)
+  def initialize(title="default", author="default")
     @title = title
     @author = author
   end
